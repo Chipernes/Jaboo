d58d8e44e86e601a2ca43ab28ccc347b4985c4d4
Chipernes <nikonprofiz@gmail.com>
2023-09-13 14:19:27 +0300

    Fixed header list view

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,8 +1,33 @@
 // Burger menu
 const burger = document.querySelector('.icon-menu');
 const menuList = document.querySelector('.menu__list');
+const menuPersonalisation = document.querySelector('.menu__personalisation');
 
 burger?.addEventListener('click', () => {
     burger.classList.toggle('menu-open');
     menuList.classList.toggle('menu-open');
+    menuPersonalisation.classList.toggle('menu-open');
 });
+
+// Changing of list
+const mediaQueryTransfer = window.matchMedia('(max-width: 425px)');
+window.addEventListener('DOMContentLoaded', checkWidth);
+mediaQueryTransfer.addEventListener('change', updateMenu);
+
+function updateMenu() {
+    if (window.innerWidth <= 425) {
+        checkWidth();
+    } else {
+        menuList.querySelectorAll('.menu__popup').forEach((item) => item.remove());
+        menuList.insertAdjacentHTML('afterend', menuPersonalisation.outerHTML);
+    }
+}
+
+function checkWidth() {
+    if (window.innerWidth <= 425) {
+        menuList.insertAdjacentHTML('beforeend', menuPersonalisation.innerHTML);
+        document.querySelector('.menu__personalisation').remove();
+    }
+}
+
+

7511bbac96ff479131c953a581a942185c8e6005
Chipernes <nikonprofiz@gmail.com>
2023-09-13 12:23:53 +0300

    Hero block was done

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,9 +2,7 @@
 const burger = document.querySelector('.icon-menu');
 const menuList = document.querySelector('.menu__list');
 
-if (burger) {
-    burger.addEventListener('click', () => {
-       burger.classList.toggle('menu-open');
-       menuList.classList.toggle('menu-open');
-    });
-}
+burger?.addEventListener('click', () => {
+    burger.classList.toggle('menu-open');
+    menuList.classList.toggle('menu-open');
+});
